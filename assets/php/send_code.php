803e1872bf89982280cdeaaf8502de05c3b32bb2
ummekulsoom599 <ummekulsoom599@gmail.com>
2023-06-11 09:59:37 +0400

    Update send_code.php

--- assets/php/send_code.php
+++ assets/php/send_code.php
@@ -23,7 +23,7 @@ global $mail;
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'itzkoalaki@gmail.com';                     //SMTP username
-        $mail->Password   = 'Ko@L@142212';                               //SMTP password
+        $mail->Password   = 'Ko******';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
